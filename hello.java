1aa3e7b17c3b2ab3a64d9311e7780a777c3f32ae
DharamVishal31 <130269461+DharamVishal31@users.noreply.github.com>
2023-07-21 10:20:27 +0100

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("Hello, Vishal!")
-        System.out.println("Hello, Bondili!"); 
+        System.out.println("Hello, Vishal!"); 
     }
 }
